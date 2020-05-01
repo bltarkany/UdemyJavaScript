22ff4af362f390b89bdb7914ec9d3db45561865e
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-05-01 14:18:43 -0700

    added javascript types and values to external javascript file.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,29 @@
 console.log("Hello World!!!");
+
+// variables are a store piece of information that can be called upon multiple times
+
+// 5 variables types: Number, String, Boolean, Undefined, null
+
+
+// String is designated by ''
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+// Number is always floating with decimal even when not specified
+var age = 28;
+console.log(age);
+
+// Boolean is either true or false
+var fullAge = true;
+console.log(fullAge);
+
+// Undefined is a variable with no value
+var job;
+console.log(job);
+
+// Null is a empty or non-existent value
+var salary = null;
+console.log(salary);
